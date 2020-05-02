50802271d4f3527800cd55120767d9a1bd982899
Bhupendra <bhupendra1011@gmail.com>
2020-05-02 20:57:00 +0530

    init folder structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { Cards, CountryPicker, Chart } from "./components";
+import styles from "./App.module.css";
+import { fetchData } from "./api";
 
 function App() {
+  useEffect(() => {
+    async function loadData() {
+      const obj = await fetchData();
+      console.log(obj);
+    }
+    loadData();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.container}>
+      <Cards />
+      <CountryPicker />
+      <Chart />
     </div>
   );
 }
-
 export default App;
